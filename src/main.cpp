23c73a399884b9193125251e76e1362559655667
kotek <scpktk@gmail.com>
2025-02-26 23:33:04 +0900

    ラムダ式のキャプチャリストを調整

--- src/main.cpp
+++ src/main.cpp
@@ -14,11 +14,11 @@ constexpr uint32_t SERIAL_READ_INTERVAL = 100;
 std::unique_ptr<bt_communication::BtCommunicator> bt_communicator(new bt_communication::BtCommunicator());
 ///  Bluetoothでいろいろやり取りする関数をまとめたクラス
 std::unique_ptr<bt_communication::BtInterface> bt_interface(new bt_communication::BtInterface(
-    [](String text) { bt_communicator->remote_print(text); },
-    [](uint8_t c620_id, float angle, int16_t rpm, int16_t amp, uint8_t temp) {
+    [&](String text) { bt_communicator->remote_print(text); },
+    [&](m3508_control::C620Id c620_id, float angle, int16_t rpm, int16_t amp, uint8_t temp) {
         bt_communicator->remote_send_m3508_feedback(c620_id, angle, rpm, amp, temp);
     },
-    [](uint8_t c620_id, float output, float p, float i, float d, float target_rpm, float error) {
+    [&](m3508_control::C620Id c620_id, float output, float p, float i, float d, float target_rpm, float error) {
         bt_communicator->remote_send_m3508_pid_fields(c620_id, output, p, i, d, target_rpm, error);
     }
 ));
