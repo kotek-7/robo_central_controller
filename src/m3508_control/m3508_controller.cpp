7432c2c3248310108701a746de625f59c4b1aff5
kotek <scpktk@gmail.com>
2025-02-27 10:53:51 +0900

    pid_controllersの初期化部を実装

--- src/m3508_control/m3508_controller.cpp
+++ src/m3508_control/m3508_controller.cpp
@@ -14,11 +14,50 @@ namespace m3508_control {
     constexpr float CLAMPING_OUTPUT = 2000;
 
     M3508Controller::M3508Controller(const bt_communication::BtInterface &bt_interface)
-        : pid_controller(
-              KP, KI, KD, CLAMPING_OUTPUT, bt_interface.remote_print,
-              [&](float output, float proportional, float integral, float derivative, float target_rpm, float error) {
-                  bt_interface.remote_send_pid_fields(C620Id::C1, output, proportional, integral, derivative, target_rpm, error);
-              }
+        : pid_controllers({
+              {C620Id::C1,
+               pid_controller::PIDController(
+                   KP, KI, KD, CLAMPING_OUTPUT, bt_interface.remote_print,
+                   [&](float output, float proportional, float integral, float derivative, float target_rpm,
+                       float error) {
+                       bt_interface.remote_send_pid_fields(
+                           C620Id::C1, output, proportional, integral, derivative, target_rpm, error
+                       );
+                   }
+               )},
+              {C620Id::C2,
+               pid_controller::PIDController(
+                   KP, KI, KD, CLAMPING_OUTPUT, bt_interface.remote_print,
+                   [&](float output, float proportional, float integral, float derivative, float target_rpm,
+                       float error) {
+                       bt_interface.remote_send_pid_fields(
+                           C620Id::C2, output, proportional, integral, derivative, target_rpm, error
+                       );
+                   }
+               )},
+              {C620Id::C3,
+               pid_controller::PIDController(
+                   KP, KI, KD, CLAMPING_OUTPUT, bt_interface.remote_print,
+                   [&](float output, float proportional, float integral, float derivative, float target_rpm,
+                       float error) {
+                       bt_interface.remote_send_pid_fields(
+                           C620Id::C3, output, proportional, integral, derivative, target_rpm, error
+                       );
+                   }
+               )},
+              {C620Id::C4,
+               pid_controller::PIDController(
+                   KP, KI, KD, CLAMPING_OUTPUT, bt_interface.remote_print,
+                   [&](float output, float proportional, float integral, float derivative, float target_rpm,
+                       float error) {
+                       bt_interface.remote_send_pid_fields(
+                           C620Id::C4, output, proportional, integral, derivative, target_rpm, error
+                       );
+                   }
+               )},
+          }),
+          pid_controller(
+              KP, KI, KD, CLAMPING_OUTPUT, bt_interface.remote_print, [](float, float, float, float, float, float) {}
           ),
           command_currents{0, 0, 0, 0},
           bt_interface(bt_interface) {}
