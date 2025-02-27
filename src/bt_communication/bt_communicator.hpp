f24e459549686a39af6f91cc4b52f7b164a88fef
kotek <scpktk@gmail.com>
2025-02-27 10:16:06 +0900

    ヘッダファイルの型も変更

--- src/bt_communication/bt_communicator.hpp
+++ src/bt_communication/bt_communicator.hpp
@@ -33,7 +33,7 @@ namespace bt_communication {
         joystick_input::JoystickInput get_joystick_r_input() const { return joystick_r_input; }
 
         void remote_print(String text);
-        void remote_send_m3508_feedback(uint8_t c620_id, float angle, int16_t rpm, int16_t amp, uint8_t temp);
+        void remote_send_m3508_feedback(m3508_control::C620Id c620_id, float angle, int16_t rpm, int16_t amp, uint8_t temp);
         void remote_send_m3508_pid_fields(uint8_t c620_id, float output, float p, float i, float d, float target_rpm, float error);
         void add_write_event_listener(std::function<void(JsonDocument doc)> listener);
 
