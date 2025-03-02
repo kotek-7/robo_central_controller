42cf622cb2448a8251672dc18db4059628bd4c85
kotek <scpktk@gmail.com>
2025-03-02 10:13:29 +0900

    Bluetoothロジックをライブラリとして切り出し(まだ未使用)

---
+++ lib/bt_communication/src/core/bt_communicator.hpp
@@ -0,0 +1,64 @@
+#pragma once
+
+#include <Arduino.h>
+#include <BLE2902.h>
+#include <BLEDevice.h>
+#include <BLEServer.h>
+#include <BLEUtils.h>
+#include <ArduinoJson.h>
+#include <memory>
+#include "m3508_control/values/c620_id.hpp"
+#include "../values/joystick_input.hpp"
+
+namespace bt_communication {
+    /// @brief Bluetooth通信を行うクラス
+    /// @details
+    ///     このクラスは、コントローラ・モニタとのBLE(Bluetooth Low Energy)通信を行います。
+    ///
+    ///     BLEでコントローラからのジョイスティック入力を自動で受け取り、保持します。
+    ///     また、モニタにBLEで各種データを送信するための関数も提供します。
+    ///     Bluetooth通信の受信時のイベントハンドラを登録することもできます。
+    ///
+    ///     使う前にsetup()を呼び出して初期化する必要があります。
+    class BtCommunicator {
+    public:
+        BtCommunicator();
+        void setup();
+
+        /// @brief Bluetoothデバイスが(1つ以上)接続されているか
+        bool is_device_connected() const { return device_connected; }
+        /// @brief 左のジョイスティック入力を取得
+        joystick_input::JoystickInput get_joystick_l_input() const { return joystick_l_input; }
+        /// @brief 右のジョイスティック入力を取得
+        joystick_input::JoystickInput get_joystick_r_input() const { return joystick_r_input; }
+
+        void remote_print(String text);
+        void
+        remote_send_m3508_feedback(m3508_control::C620Id c620_id, float angle, int16_t rpm, int16_t amp, uint8_t temp);
+        void remote_send_m3508_pid_fields(
+            m3508_control::C620Id c620_id, float output, float p, float i, float d, float target_rpm, float error
+        );
+        void add_write_event_listener(String type, std::function<void(JsonDocument doc)> listener);
+
+    private:
+        /// @brief bluetoothデバイスが(1つ以上)接続されているか
+        bool device_connected;
+        /// @brief BLEサーバーへのポインタ
+        std::unique_ptr<BLEServer> ble_server;
+        /// @brief 送信用Characteristicへのポインタ
+        std::unique_ptr<BLECharacteristic> tx_characteristic;
+        /// @brief 受信用Characteristicへのポインタ
+        std::unique_ptr<BLECharacteristic> rx_characteristic;
+        /// @brief 左のジョイスティック入力
+        joystick_input::JoystickInput joystick_l_input;
+        /// @brief 右のジョイスティック入力
+        joystick_input::JoystickInput joystick_r_input;
+        std::vector<std::pair<String, std::function<void(JsonDocument doc)>>> on_write_event_listeners;
+
+        void on_connect(BLEServer *server);
+        void on_disconnect(BLEServer *server);
+        void on_write(BLECharacteristic *characteristic);
+        void remote_send_joystick_input(joystick_input::JoystickInput joystick_input, String side);
+        void set_and_forward_joystick_input(JsonDocument doc);
+    };
+} // namespace bt_communication
---
+++ lib/bt_communication/src/values/joystick_input.cpp
@@ -0,0 +1,11 @@
+#include <Arduino.h>
+#include "utils/vec2.hpp"
+#include "joystick_input.hpp"
+
+namespace bt_communication::joystick_input {
+    JoystickInput::JoystickInput()
+        : input(utils::Vec2(0, 0)), leveled_input(utils::Vec2(0, 0)), distance(0), angle(0) {}
+
+    JoystickInput::JoystickInput(utils::Vec2 input, utils::Vec2 leveled_input, float distance, float angle)
+        : input(input), leveled_input(leveled_input), distance(distance), angle(angle) {}
+} // namespace bt_communication::joystick_input
