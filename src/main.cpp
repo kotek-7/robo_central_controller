0e7bc3fe7f24d615b42231001ddf27f8a30a45f7
kotek <scpktk@gmail.com>
2025-03-07 02:52:34 +0900

    制御周期を高速化

--- src/main.cpp
+++ src/main.cpp
@@ -6,9 +6,9 @@
 #include <mpu6050_control/core.hpp>
 
 /// @brief M3508へのCANの送信間隔(=PIDの制御周期)[ms]
-constexpr uint32_t M3508_SEND_INTERVAL = 20;
+constexpr uint32_t M3508_SEND_INTERVAL = 10;
 /// @brief M3508からのCANの受信(=PIDのFB受信間隔)間隔[ms]
-constexpr uint32_t M3508_RECEIVE_INTERVAL = 20;
+constexpr uint32_t M3508_RECEIVE_INTERVAL = 10;
 /// @brief CAN通信の受信間隔[ms]
 constexpr uint32_t CAN_RECEIVE_INTERVAL = 100;
 /// @brief シリアル通信の読み取り間隔[ms]
