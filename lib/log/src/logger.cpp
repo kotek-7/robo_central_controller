2a0864195e3c24ffaf54b88f7f4d6427a7536e4a
kotek <scpktk@gmail.com>
2025-03-07 22:40:57 +0900

    ロギングクラスにconstを追加

--- lib/log/src/logger.cpp
+++ lib/log/src/logger.cpp
@@ -4,43 +4,43 @@ namespace log {
     Logger::Logger(bool enabled = true, bt_communication::BtPrinter &bt_printer) :
         enabled(enabled), bt_printer(bt_printer) {}
 
-    void Logger::println(const char *message) {
+    void Logger::println(const char *message) const {
         Serial.println(message);
         bt_printer.remote_print(message);
     }
 
-    void Logger::println(const String message) {
+    void Logger::println(const String message) const {
         println(message.c_str());
     }
-    void Logger::println(const uint64_t message) {
+    void Logger::println(const uint64_t message) const {
         println(String(message));
     }
 
-    void Logger::println(const int64_t message) {
+    void Logger::println(const int64_t message) const {
         println(String(message));
     }
 
-    void Logger::println(const uint32_t message) {
+    void Logger::println(const uint32_t message) const {
         println(String(message));
     }
-    void Logger::println(const int32_t message) {
+    void Logger::println(const int32_t message) const {
         println(String(message));
     }
-    void Logger::println(const uint16_t message) {
+    void Logger::println(const uint16_t message) const {
         println(String(message));
     }
-    void Logger::println(const int16_t message) {
+    void Logger::println(const int16_t message) const {
     }
-    void Logger::println(const uint8_t message) {
+    void Logger::println(const uint8_t message) const {
         println(String(message));
     }
-    void Logger::println(const int8_t message) {
+    void Logger::println(const int8_t message) const {
         println(String(message));
     }
-    void Logger::println(const float message) {
+    void Logger::println(const float message) const {
         println(String(message));
     }
-    void Logger::println(const double message) {
+    void Logger::println(const double message) const {
         println(String(message));
     }
 } // namespace log
--- lib/log/src/logger.hpp
+++ lib/log/src/logger.hpp
@@ -7,18 +7,18 @@ namespace log {
     class Logger {
     public:
         Logger(bool enabled = true, bt_communication::BtPrinter &bt_printer);
-        void println(const char *message);
-        void println(const String message);
-        void println(const uint64_t message);
-        void println(const int64_t message);
-        void println(const uint32_t message);
-        void println(const int32_t message);
-        void println(const uint16_t message);
-        void println(const int16_t message);
-        void println(const uint8_t message);
-        void println(const int8_t message);
-        void println(const float message);
-        void println(const double message);
+        void println(const char *message) const;
+        void println(const String message) const;
+        void println(const uint64_t message) const;
+        void println(const int64_t message) const;
+        void println(const uint32_t message) const;
+        void println(const int32_t message) const;
+        void println(const uint16_t message) const;
+        void println(const int16_t message) const;
+        void println(const uint8_t message) const;
+        void println(const int8_t message) const;
+        void println(const float message) const;
+        void println(const double message) const;
 
     private:
         bool enabled;
